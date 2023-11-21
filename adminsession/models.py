be20692a5cfd46b6299ba12c1860d36d21de3091
Sanat <you@example.com>
2023-11-21 15:40:57 +0530

    alter sessionName Length

--- adminsession/models.py
+++ adminsession/models.py
@@ -5,7 +5,7 @@ from auth_app.models import AdminModel
 
 class SessionModel(models.Model, object):
     user = models.ForeignKey(AdminModel, on_delete=models.CASCADE, related_name='%(app_label)s_%(class)s_related')
-    session_name = models.CharField(max_length=32)
+    session_name = models.CharField(max_length=255)
     branch_data = models.JSONField()
     created_on = models.DateTimeField(auto_now_add=True)
     report_link = models.CharField(max_length=255, null=True, blank=True)
